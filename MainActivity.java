83792266b308659b1e2aa403484c320c8e51d2ae
cho seong bin <92620088+paulsea27115@users.noreply.github.com>
2022-12-09 00:12:28 +0900

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.in_kakaomap;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    MapView mMapView = new MapView(this);
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+
+}
